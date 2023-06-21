b5d42af2e4da585c9a134a6ab241c19523500165
Yuliia <yulja01@gmail.com>
2023-06-21 16:48:42 -0500

    connected js and css files links

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
-alert('Hello world');
-const favoriteFood = 'Thai';
-document.write(favoriteFood);
+let pokemonList = [];
+
+// First Pokémon object
+let pokemon1 = {
+    name: "Pikachu",
+    height: 4,
+    types: ['electric']
+};
+
+// Second Pokémon object
+let pokemon2 = {
+    name: "Charizard",
+    height: 17,
+    types: ['fire', 'flying']
+};
+
+// Third Pokémon object
+let pokemon3 = {
+    name: "Squirtle",
+    height: 5,
+    types: ['water']
+};
+
+// Fourth Pokémon object
+let pokemon5 = {
+    name: "Jigglypuff",
+    height: 5,
+    types: ['normal', 'fairy']
+};
+
+// Adding Pokémon objects to the pokemonList array
+pokemonList.push(pokemon1);
+pokemonList.push(pokemon2);
+pokemonList.push(pokemon3);
+console.log(pokemonList)
