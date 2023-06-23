1b60a935270ddf3bfc9ba667319e49451c09d23c
Yuliia <yulja01@gmail.com>
2023-06-23 15:27:56 -0500

    changed variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,24 @@
-let pokemonList = [];
+let pokemonList = [
+    {
+        name: "Pikachu",
+        height: 4,
+        types: ['electric']
+    },
+    {
+        name: "Charizard",
+        height: 17,
+        types: ['fire', 'flying']
+    },
+    {
+        name: "Squirtle",
+        height: 5,
+        types: ['water']
+    },
+    {
+        name: "Jigglypuff",
+        height: 5,
+        types: ['normal', 'fairy']
+    }
+];
 
-// First Pokémon object
-let pokemon1 = {
-    name: "Pikachu",
-    height: 4,
-    types: ['electric']
-};
-
-// Second Pokémon object
-let pokemon2 = {
-    name: "Charizard",
-    height: 17,
-    types: ['fire', 'flying']
-};
-
-// Third Pokémon object
-let pokemon3 = {
-    name: "Squirtle",
-    height: 5,
-    types: ['water']
-};
-
-// Fourth Pokémon object
-let pokemon5 = {
-    name: "Jigglypuff",
-    height: 5,
-    types: ['normal', 'fairy']
-};
-
-// Adding Pokémon objects to the pokemonList array
-pokemonList.push(pokemon1);
-pokemonList.push(pokemon2);
-pokemonList.push(pokemon3);
 console.log(pokemonList);
