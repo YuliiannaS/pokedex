3798daf14002888edbe4030bac7b7864bfe2112b
Yuliia <yulja01@gmail.com>
2023-06-27 15:34:25 -0500

    formatated document ended task

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const pokemonRepository = (function (){
+const pokemonRepository = (function () {
     let pokemonList = [
         {
             name: "Pikachu",
@@ -15,58 +15,58 @@ const pokemonRepository = (function (){
             height: 5,
             types: ['water']
         },
-      ];
-    
+    ];
+
     // Show pokemon details
     function showDetails(pokemon) {
-      console.log(pokemon);
+        console.log(pokemon);
     }
-    
+
     return {
-    // Get all pokemons
-        getAll: function() {
-          return pokemonList;
-      },
-      // Add pokemon
-      add: function(pokemon) {
-        if (typeof pokemon !== "object") {
-          return false;
-        }
-        if (typeof pokemon.height !== "number") {
-          return false;
-        }
-        if (typeof pokemon.name !== "string") {
-        return false;
-        }
-          pokemonList.push(pokemon);
-      },
-      // Get one pokemon by name
-      filter: function(name) {
-        pokemonList.find(function(pokemon) {
-          return pokemon.name === name;
-        })
-      },
-      // Add button to the list
-      addListItem: function(pokemon) {
-          const ul = document.querySelector("ul");
-        const li = document.createElement("li");
-        const button = document.createElement("button");
-        
-        button.innerText = pokemon.name;
-        // Addd click listener
-        button.addEventListener("click", function() {
-          showDetails(pokemon);
-        });
-        
-        li.appendChild(button);
-        ul.appendChild(li);
-      },
+        // Get all pokemons
+        getAll: function () {
+            return pokemonList;
+        },
+        // Add pokemon
+        add: function (pokemon) {
+            if (typeof pokemon !== "object") {
+                return false;
+            }
+            if (typeof pokemon.height !== "number") {
+                return false;
+            }
+            if (typeof pokemon.name !== "string") {
+                return false;
+            }
+            pokemonList.push(pokemon);
+        },
+        // Get one pokemon by name
+        filter: function (name) {
+            pokemonList.find(function (pokemon) {
+                return pokemon.name === name;
+            })
+        },
+        // Add button to the list
+        addListItem: function (pokemon) {
+            const ul = document.querySelector("ul");
+            const li = document.createElement("li");
+            const button = document.createElement("button");
+
+            button.innerText = pokemon.name;
+            // Addd click listener
+            button.addEventListener("click", function () {
+                showDetails(pokemon);
+            });
+
+            li.appendChild(button);
+            ul.appendChild(li);
+        },
     };
-  })();
-  
-  // Get all pokemons
-  const pokemons = pokemonRepository.getAll();
-  // Iterate and print all pokemons
-  pokemons.forEach(function (pokemon) {
+})();
+
+// Get all pokemons
+const pokemons = pokemonRepository.getAll();
+// Iterate and print all pokemons
+pokemons.forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
-  });
+});
