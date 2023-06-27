276207ca51321098c0922c1cbca397691a2cc059
Yuliia <yulja01@gmail.com>
2023-06-27 13:20:32 -0500

    replaced for with forEach; added getAll , filter and add methods.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,76 @@
-let pokemonList = [
-    {
-        name: "Pikachu",
-        height: 4,
-        types: ['electric']
-    },
-    {
-        name: "Charizard",
-        height: 17,
-        types: ['fire', 'flying']
-    },
-    {
-        name: "Squirtle",
-        height: 5,
-        types: ['water']
-    },
-    {
+const pokemonRepository = (function (){
+    let pokemonList = [
+      {
+          name: "Pikachu",
+          height: 4,
+          types: ['electric']
+      },
+      {
+          name: "Charizard",
+          height: 17,
+          types: ['fire', 'flying']
+      },
+      {
+          name: "Squirtle",
+          height: 5,
+          types: ['water']
+      },
+    ];
+    
+    return {
+        getAll: function() {
+          return pokemonList;
+      },
+      add: function(pokemon) {
+        if (typeof pokemon !== "object") {
+          return false;
+        }
+        if (typeof pokemon.height !== "number") {
+          return false;
+        }
+        if (typeof pokemon.name !== "string") {
+        return false;
+        }
+          pokemonList.push(pokemon);
+      },
+      filter: function(name) {
+        return pokemonList.find(function (pokemon) {
+          return pokemon.name === name;
+        });
+      },
+    };
+  })();
+  
+  const newPokemon = {
         name: "Jigglypuff",
         height: 5,
         types: ['normal', 'fairy']
-    }
-];
-//for loop that iterates over each item in pokemonList
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(
-        pokemonList[i].name + " (height: " + pokemonList[i].height + ")"
+      };
+  pokemonRepository.add(newPokemon);
+  const pokemons = pokemonRepository.getAll();
+  
+  pokemons.forEach(function (pokemon) {
+      document.write(
+        pokemon.name + " (height: " + pokemon.height + ")"
       );
-      if (pokemonList[i].height > 10) {
+      if (pokemon.height > 10) {
         document.write(
-          " Wow, that’s big!"
+          " Wow, that’s big"
         );
       }
       document.write("<br>");
-}
+    });
+    
+  document.write("<h1>My pokemon:</h1>")
+  const my = pokemonRepository.filter("Pikachu");
+  document.write(
+        my.name + " (height: " + my.height + ")"
+      );
+      if (my.height > 10) {
+        document.write(
+          " Wow, that’s big"
+        );
+      }
+    
 
-console.log(pokemonList);
+console.log(pokemonRepository);
