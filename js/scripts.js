35240a974b0afaa49c50454651a8a21a7b5b5c4e
Yuliia <yulja01@gmail.com>
2023-07-10 13:30:34 -0500

    task 7

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,42 @@
 const pokemonRepository = (function () {
-    let pokemonList = [
-        {
-            name: "Pikachu",
-            height: 4,
-            types: ['electric']
-        },
-        {
-            name: "Charizard",
-            height: 17,
-            types: ['fire', 'flying']
-        },
-        {
-            name: "Squirtle",
-            height: 5,
-            types: ['water']
-        },
-    ];
+    let pokemonList = [];
+       //load data from link
+    function loadList() {
+        return fetch("https://pokeapi.co/api/v2/pokemon/")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                data.results.forEach(function (pokemon) {
+                    add({
+                        name: pokemon.name,
+                        detailsUrl: pokemon.url,
+                    });
+                });
+            })
+    }
+      //Load pokemon details
+    function loadDetails(pokemon) {
+        return fetch(pokemon.detailsUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                const pokemonUpdate = pokemonList.find(function (p) {
+                    return p.name == pokemon.name;
+                });
+                pokemonUpdate.imgUrl = data.sprites.front_default;
+                pokemonUpdate.height = data.height;
+                return pokemonUpdate;
+            });
+    }
 
     // Show pokemon details
     function showDetails(pokemon) {
-        console.log(pokemon);
+        return loadDetails(pokemon)
+            .then(function (data) {
+                console.log(data);
+            });
     }
     // Get all pokemons
     function getAll() {
@@ -30,9 +47,9 @@ const pokemonRepository = (function () {
         if (typeof pokemon !== "object") {
             return false;
         }
-        if (typeof pokemon.height !== "number") {
-            return false;
-        }
+        // if (typeof pokemon.height !== "number") {
+        //     return false;
+        // }
         if (typeof pokemon.name !== "string") {
             return false;
         }
@@ -60,16 +77,23 @@ const pokemonRepository = (function () {
         ul.appendChild(li);
     }
     return {
-        getAll: getAll, addListItem: addListItem
-
-
-
+        getAll: getAll,
+        addListItem: addListItem,
+        loadList: loadList,
     };
 }) ();
 
 // Get all pokemons
-const pokemons = pokemonRepository.getAll();
-// Iterate and print all pokemons
-pokemons.forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList()
+ .then(function() {
+    const pokemons = pokemonRepository.getAll();
+    // Iterate and print all pokemons
+    pokemons.forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
+ })
+ .catch(function (error) {
+    console.error(error);
 });
+
+
